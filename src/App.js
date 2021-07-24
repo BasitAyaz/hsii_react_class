721452593ba4292a129aab1cfab951d880a6266d
BasitAyaz <basitahmed1997@gmail.com>
2021-07-24 16:57:37 +0500

    24-07-2021

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   const [nam, setNam] = useState("Basit");
   const [num, newNum] = useState(0);
   const [email, setEmail] = useState("");
+  const [arr, setArr] = useState(["a", "b", "c"]);
 
   return (
     <div className="App">
@@ -15,6 +16,10 @@ function App() {
         <h2>{email}</h2>
         <input onChange={(a) => setEmail(a.target.value)} />
         <button onClick={() => newNum(num + 1)}>+</button>
+
+        {arr.map((e, i) => {
+          return <p key={i}>{e}</p>;
+        })}
       </header>
     </div>
   );
